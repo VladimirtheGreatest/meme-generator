598552d2a503d587620b62e0055b8fdf194fe1d2
Vladimir <48209956+VladimirtheGreatest@users.noreply.github.com>
2019-07-26 20:53:18 +0100

    Create header.js

---
+++ header.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+function Header() {
+    return (
+        <header>
+            <img 
+                src="https://www.lifewire.com/thmb/1p3mrM5_hSoIFQUfh53VK1q4dvg=/768x0/filters:no_upscale():max_bytes(150000):strip_icc()/dicaprio3-583e33155f9b58d5b19e3a00.jpg" 
+                alt="lol"
+            />
+            <p>Meme Generator</p>
+        </header>
+    )
+}
+
+export default Header
