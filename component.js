1f29c82d4a3f02d3783da312077902911454a49a
Vladimir <48209956+VladimirtheGreatest@users.noreply.github.com>
2019-07-26 20:56:29 +0100

    Create component.js

---
+++ component.js
@@ -0,0 +1,26 @@
+import React, {Component} from "react"
+
+
+class Generator extends Component {
+    constructor() {
+        super()
+        this.state = {
+        }
+    }
+
+render() {
+        return (
+            <div>
+                <form>
+                </form>
+                <div>
+                </div>
+            </div>
+        )
+    }
+}
+
+
+
+
+export default MemeGenerator
