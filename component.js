7f4ce46594a65cd76d257975a1f8d0bc36861475
Vladimir <48209956+VladimirtheGreatest@users.noreply.github.com>
2019-07-27 10:45:30 +0100

    Update component.js

--- component.js
+++ component.js
@@ -5,7 +5,20 @@ class Generator extends Component {
     constructor() {
         super()
         this.state = {
+             topText: "",
+            bottomText: "",
+            randomImg: "http://i.imgflip.com/1bij.jpg",
+            allMemeImgs: []
         }
+    }
+    
+     componentDidMount() {
+        fetch("https://api.imgflip.com/get_memes")
+            .then(response => response.json())
+            .then(response => {
+                const {memes} = response.data
+                this.setState({ allMemeImgs: memes })
+            })
     }
 
 render() {
