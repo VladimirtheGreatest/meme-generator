9caaca682491ffdf46ec88c30a9bd5af44210421
Vladimir <48209956+VladimirtheGreatest@users.noreply.github.com>
2019-07-28 09:08:54 +0100

    Update component.js

--- component.js
+++ component.js
@@ -4,29 +4,53 @@ import React, {Component} from "react"
 class Generator extends Component {
     constructor() {
         super()
-        this.state = {
-             topText: "",
+        this.state = {   // initial state, no text, initial image, empty array to receive data from API
+            topText: "",
             bottomText: "",
             randomImg: "http://i.imgflip.com/1bij.jpg",
             allMemeImgs: []
         }
     }
     
-     componentDidMount() {
+     componentDidMount() {   //mounting component, fetching image from the api
         fetch("https://api.imgflip.com/get_memes")
             .then(response => response.json())
             .then(response => {
                 const {memes} = response.data
-                this.setState({ allMemeImgs: memes })
+                this.setState({ allMemeImgs: memes })  // changing state
             })
+    }
+    
+       handleChange(event) {    //handling any changes to the input
+        const {name, value} = event.target
+        this.setState({ [name]: value })
     }
 
 render() {
         return (
-            <div>
-                <form>
+             <div>
+                <form onSubmit={this.handleSubmit}>
+                    <input 
+                        type="text"
+                        name="topText"
+                        placeholder="Top Text"
+                        value={this.state.topText}
+                        onChange={this.handleChange}
+                    /> 
+                    <input 
+                        type="text"
+                        name="bottomText"
+                        placeholder="Bottom Text"
+                        value={this.state.bottomText}
+                        onChange={this.handleChange}
+                    /> 
+                
+                    <button>Gen</button>
                 </form>
                 <div>
+                    <img src={this.state.randomImg} alt="" />
+                    <h2 className="top">{this.state.topText}</h2>
+                    <h2 className="bottom">{this.state.bottomText}</h2>
                 </div>
             </div>
         )
